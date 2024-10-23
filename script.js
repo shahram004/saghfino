a6efe1695b673ae7b0c9da4a468c7985da4c65c1
shahram004 <160882470+shahram004@users.noreply.github.com>
2024-10-23 19:51:42 +0330

    debug btn

--- script.js
+++ script.js
@@ -6,9 +6,7 @@ const closeMenu =$.getElementById('closMenu')
 let menuOpen = false
 openMenuBtn.addEventListener ('click', function () {
       mobileMenu.style.display = 'block'
-      // header.style.display = 'none'
     })
     closeMenu.addEventListener('click',()=>{
         mobileMenu.style.display = 'none'
-        // header.style.display = 'block'
 })
